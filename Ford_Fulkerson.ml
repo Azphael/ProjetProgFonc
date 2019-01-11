f9bf1937c3a37183ab50528d6bb7364b5d74377b
O.Rocher <olivier.rocher.1313@gmail.com>
2019-01-11 01:31:19 +0100

    path are 'a graph now, need work on final ff function

--- Ford_Fulkerson.ml
+++ Ford_Fulkerson.ml
@@ -3,6 +3,7 @@ open Graph
 
 (** Algo Ford Fulkerson **)
 
+(**
 (** Fonctions sur les paths **)
 let out_arcs_path path id =
   try List.assoc id path
@@ -13,7 +14,7 @@ let find_arc_path path id1 id2 =
   let out = out_arcs_path path id1 in
     try Some (List.assoc id2 out)
     with Not_found -> None
-
+**)
 	
 (** Fonctions de recherche de chemin **)	
 (** Fonction principale renvoyant un chemin sous forme de liste de noeud assoyé à son arc sortant **)
@@ -31,39 +32,6 @@ let rec search_path graph acu localisation target noeuds_dest =
 						end
 				end
 **)
-				
-let rec search_path graph path localisation target noeuds_dest = 
-	if localisation = target then path
-		else let arcs_potentiel = List.filter ( fun (y, (z, v)) -> (v < z) ) noeuds_dest in
-				begin match arcs_potentiel with
-					| [] -> path
-					| (y, (z, v)) :: tl -> 
-						begin match search_path graph (add_arc path localisation y (z,v)) y target (out_arcs graph y) with
-							| [] -> search_path graph path localisation target tl
-							| path -> path
-						end
-				end				
-
-
-				
-				
-let 2 = search_path
-				
-
-(** Fonction de recherche de chemin "User Friendly" **)				
-let find_path graph source target =
-	search_path graph [] source target ( (fun x -> out_arcs graph x) source)
-	
-
-(** Lecture de la valeur maximale de flux pouvant passer dans le chemin trouvé **)
-let max_flux_passant path =
-	let rec loop_fluxmax mini path =
-		match path with
-			| [] -> mini
-			| (x, (y, (z, v))) :: tl -> if (v < mini) then loop_fluxmax v tl
-				else loop_fluxmax mini tl
-	in
-		loop_fluxmax max_int path
 
 
 (**
@@ -79,24 +47,102 @@ let give_flow_max graph target =
 		loop_maxflow 0 graph target
 **)
 
-(** Mise à jour du graph d'écart lié **)
+
+(** 				
+let rec search_path graph path localisation target noeuds_dest = 
+	if localisation = target then path
+		else let arcs_potentiel = List.filter ( fun (y, (z, v)) -> (v < z) ) noeuds_dest in
+				begin match arcs_potentiel with
+					| [] -> path
+					| (y, (z, v)) :: tl -> 
+						begin match search_path graph (add_arc path localisation y (z,v)) y target (out_arcs graph y) with
+							| [] -> search_path graph path localisation target tl
+							| path -> path
+						end
+				end	
+**)
+
+let rec search_path graph path localisation target noeuds_dest =
+	if localisation = target then path
+	else
+		let arcs_potentiel = List.filter ( fun (y, (z, v)) -> (v < z) ) noeuds_dest in
+			let f path localisation arcs_potentiel = 
+				let rec loop_search_path arcs_potentiel =
+					match arcs_potentiel with
+						| [] -> path
+						| (y, (z, v)) :: tl ->
+							if ((node_exists path y) = true ) then loop_search_path tl
+							else
+								let new_arc_path = add_arc path localisation y (z,v) in
+									search_path graph new_arc_path y target (out_arcs graph y)
+				in
+					loop_search_path arcs_potentiel
+			in
+				v_fold path f graph
+
+
+(** Fonction de recherche de chemin "User Friendly" **)				
+let find_path graph source target =
+	search_path graph empty_graph source target ( (fun x -> out_arcs graph x) source)
+
+
+(** Lecture de la valeur maximale de flux pouvant passer dans le chemin trouvé **)
+(** let max_flux_passant path =
+	let rec loop_fluxmax mini path =
+		match path with
+			| [] -> mini
+			| (x, (y, (z, v))) :: tl -> if (v < mini) then loop_fluxmax v tl
+				else loop_fluxmax mini tl
+	in
+		loop_fluxmax max_int path **)
+
+
+let max_flux_passant path =
+	let f mini x listArcs =
+		let rec loop_fluxmax mini listArcs =
+			match listArcs with
+				| [] -> mini
+				| (y, (z, v)) :: tl -> if (v < mini) then loop_fluxmax v tl
+					else loop_fluxmax mini tl
+		in
+			loop_fluxmax mini listArcs
+	in
+		v_fold path f max_int
+
+
+(** Mise à jour du graph d'écart lié parcours de comparaison sur le graphe **)
 let update_graph graph path maxflux =
-	let f acu x listArcs = 
+	let f update x listArcs = 
 		let rec loop_outarcs_update listArcs =
 			match listArcs with
-				| [] -> acu
+				| [] -> update
 				| (y, (z,v)) :: tl ->
-						if ((find_arc_path path x y) = None) then loop_outarcs_update tl
+						if ((find_arc path x y) = None) then loop_outarcs_update tl
 						else
-							let acu_upd_in_dest = add_arc acu x y (z, ((fun a -> (a + maxflux)) v)) in
-								let acu_upd_out_dest = add_arc acu_upd_in_dest y x (z, ((fun (a,b) -> (a - b - maxflux)) (z, v) )) in
-									acu_upd_out_dest
+							let acu_upd_in_dest = add_arc update x y (z, (v + maxflux)) in
+								add_arc acu_upd_in_dest y x (z, (z - v - maxflux))
 		in
 			loop_outarcs_update listArcs
 	in
 		v_fold graph f graph
 
-		
+
+(** Mise à jour alternative du graph d'écart lié : parcours de comparaison sur le path **)		
+let alt_update_graph graph path maxflux =
+	let f updatedgraph x listArc =
+		let rec loop_outarcs_update listArc =
+			match listArc with
+				| [] -> updatedgraph
+				| (y, (z,v)) :: tl ->
+						let acu_upd_in_dest = add_arc updatedgraph x y (z, (v + maxflux)) in
+							add_arc acu_upd_in_dest y x (z, (z - v - maxflux))
+		in
+			loop_outarcs_update listArc
+	in
+		v_fold path f graph
+
+
+(** Suppression dans le graphe solution des arcs non présents dans le graphe d'origine **)		
 let graphe_sexy graph_origine graph_fin =
 	let f acu x listArcs =
 		let rec loop_outarcs_compare listArcs =
@@ -123,11 +169,21 @@ let resolve_ford_fulkerson graph source puit =
 		in
 			resolve_loop graph_modifie
 
+			
+(** let resolve_ford_fulkerson graph source puit =
+	let graph_modifie = map graph ( fun z -> (z, 0) ) in
+		let rec loop_resolve_ff acu **)
+			
+			
+			
+			
 let 5 = resolve_ford_fulkerson
 					
 
 (** Fonction user friendly renvoyant un graphe solution selon la méthode Ford-Fulkerson **)
-let ford_fulkerson graph source puit = graphe_sexy (resolve_ford_fulkerson graphe source puit) graph
+let ford_fulkerson graph source puit =
+	let graph_fin = resolve_ford_fulkerson graphe source puit in
+		graphe_sexy graph graph_fin
 					
 
 let 6 = ford_fulkerson
--- Ford_Fulkerson.mli
+++ Ford_Fulkerson.mli
@@ -1,33 +1,38 @@
 (** Alg Ford Fulkerson **)
 
-val find_arc_path: ('a * 'b) list -> 'a -> 'b
-
-val out_arcs_path: ('a * ('b * 'c) list) list -> 'a -> 'b -> 'c option
-
 (** Recherche le chemin dans un graphe entre un noeud initial et une cible finale, et renvoi **)
 (** val search_path: ('a * 'a) Graph.graph ->
 					(Graph.id * (Graph.id * ('a * 'a))) list ->
 					Graph.id ->
 					Graph.id ->
 					('a * 'a) Graph.out_arcs -> (Graph.id * (Graph.id * ('a * 'a))) list **)
-val search_path: int					
+val search_path: ('a * 'a) Graph.graph ->
+         ('a * 'a) Graph.graph ->
+         Graph.id ->
+         Graph.id -> ('a * 'a) Graph.out_arcs -> ('a * 'a) Graph.graph					
 			
 (** Trouve le chemin dans un graphe entre une "source" et un "puit" **)		   
+(** val find_path: ('a * 'a) Graph.graph ->
+         Graph.id -> Graph.id -> (Graph.id * (Graph.id * ('a * 'a))) list **)
 val find_path: ('a * 'a) Graph.graph ->
-         Graph.id -> Graph.id -> (Graph.id * (Graph.id * ('a * 'a))) list
+         Graph.id -> Graph.id -> ('a * 'a) Graph.graph
 		   
 (** Valeur minimale de flux pouvant passer dans le chemin trouvé **)
-val max_flux_passant: ('a * ('b * ('c * int))) list -> int
+(** val max_flux_passant: ('a * ('b * ('c * int))) list -> int **)
+
+val max_flux_passant: ('a * 'a) Graph.graph -> int
 
 (** Fonction renvoyantla valeur du Flow max circulant dans le graphe **)
-val give_flow_max: ('a * ('b * ('c * int))) list -> 'b -> int
+val give_flow_max: int
 
 (** Mise à jour du graph d'écart lié **)
 (** val update_graph: ('a * 'a) Graph.graph ->
          'a Graph.graph -> 'a -> ('a * 'a) Graph.graph **)
 val update_graph: ('a * 'a) Graph.graph ->
-         (Graph.id * (Graph.id * 'b) list) list ->
-         int -> ('a * 'a) Graph.graph
+         'a Graph.graph -> int -> ('a * 'a) Graph.graph
+		 
+val alt_update_graph: ('a * 'a) Graph.graph ->
+         ('a * 'a) Graph.graph -> int -> ('a * 'a) Graph.graph
 
 (** Fonction renvoyant un graphe sans les arcs retour du graphe d'écart  **)
 val graphe_sexy:  ('a * 'b) Graph.graph -> 'c Graph.graph -> ('a * 'b) Graph.graph
@@ -38,11 +43,3 @@ val resolve_ford_fulkerson: int
 (** Fonction user friendly renvoyant un graphe solution selon la méthode Ford-Fulkerson **)
 val ford_fulkerson: int
 
-
-
-
-
-
-(** Transformation du Graphe d'origine en graphe complet avec Flow max et coût passage **)
-(** val transfograph: ('a * ('b * 'c)) list ->
-					('a * ('b * ('c * int))) list -> ('a * ('b * ('c * int))) list **)
